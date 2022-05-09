42163cb70f2a0598035563d7ee75c6e3d078e8e4
Wandersonrp <wandersonrp.49@hotmail.com>
2022-05-09 18:48:24 -0300

    Department entity class

---
+++ src/aplication/TesteProgram.java
@@ -0,0 +1,14 @@
+package aplication;
+
+import model.entities.Department;
+
+public class TesteProgram {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Cars");
+		System.out.println(obj);
+
+	}
+
+}
